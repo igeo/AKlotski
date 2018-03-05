83579261b33d87ee46469e0b5c30fe60e97418a5
sbai <shufengbai@gmail.com>
2018-03-05 01:32:10 -0800

    add image to main activity

--- app/src/main/java/com/example/paul/aklotski/MainActivity.java
+++ app/src/main/java/com/example/paul/aklotski/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.paul.aklotski;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main); // use code ot set instead of xml
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.forgotmenot);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+        TextView textView1 = new TextView(this);
+        textView1.setText("Klotski");
+        textView1.setTextSize(30);
+        textView1.setGravity(Gravity.CENTER);
+        textView1.setTextColor(Color.parseColor("#f3f3f3"));
+        textView1.setTypeface(null, Typeface.BOLD);
+        textView1.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+        FrameLayout.LayoutParams lp1 = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        lp1.setMargins(0,20,0,0);
+        textView1.setLayoutParams(lp1);
+
+//Initializing frame layout
+
+        FrameLayout framelayout = new FrameLayout(this);
+        framelayout.setLayoutParams(new AbsListView.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT));
+
+//Adding views to FrameLayout
+
+        framelayout.addView(imageView);
+        framelayout.addView(textView1);
+        setContentView(framelayout);
     }
 }
