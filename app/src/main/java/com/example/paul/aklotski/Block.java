fa569c90fcee570de32ea16b7de3472636e9c3a3
sbai <shufengbai@gmail.com>
2018-03-09 23:19:02 -0800

    just show the numbers on board

---
+++ app/src/main/java/com/example/paul/aklotski/Block.java
@@ -0,0 +1,8 @@
+package com.example.paul.aklotski;
+
+/**
+ * Created by Paul on 3/9/2018.
+ */
+
+public class Block extends Object {
+}
--- app/src/main/java/com/example/paul/aklotski/MainActivity.java
+++ app/src/main/java/com/example/paul/aklotski/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.paul.aklotski;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.graphics.Color;
@@ -10,6 +11,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.widget.AbsListView;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -44,11 +46,69 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout framelayout = new FrameLayout(this);
         framelayout.setLayoutParams(new AbsListView.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                 FrameLayout.LayoutParams.MATCH_PARENT));
-
+/*
+        GridLayout gridlayout = new GridLayout(this);
+        gridlayout.setColumnCount(4);
+        gridlayout.setRowCount(4);
+        GridLayout.LayoutParams lp2 = new GridLayout.LayoutParams();
+        gridlayout.setLayoutParams(new AbsListView.LayoutParams(GridLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT));
+        for(int i = 0; i < 8; ++i) {
+            PieceLayout piece = new PieceLayout(this);
+            piece.setNum(i);
+            gridlayout.addView(piece);
+        }
+*/
 //Adding views to FrameLayout
 
-        framelayout.addView(imageView);
-        framelayout.addView(textView1);
+        for(int x = 0; x < 4; ++x)
+            for(int y = 0; y < 5; ++y)
+            {
+                PieceLayout piece = new PieceLayout(this, x, y);
+                piece.setNum(x*10+y);
+                //framelayout.addView(imageView);
+                //framelayout.addView(textView1);
+                framelayout.addView(piece);
+            }
         setContentView(framelayout);
     }
+
+
+}
+
+class PieceLayout extends FrameLayout {
+        PieceLayout(Context context, int x, int y)
+        {
+            super(context);
+            label = new TextView(getContext());
+            label.setTextSize(32);
+            label.setBackgroundColor(0x33ff0033);
+            label.setTextColor(0x330D0D0D);
+            label.setGravity(Gravity.CENTER);
+            //LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+            int W = 300;
+            int H = 300;
+            int G = 10;
+            LayoutParams lp = new LayoutParams(W, H);
+            lp.setMargins(x*(W+G), y*(H+G), (3-x)*(W+G), (4-y)*(H+G));
+            label.setLayoutParams(lp);
+            setNum(0);
+            addView(label);
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+        if (num <= 0) {
+            label.setText("");
+        } else {
+            label.setText(num + "");
+        }
+    }
+    int num;
+    TextView label;
 }
