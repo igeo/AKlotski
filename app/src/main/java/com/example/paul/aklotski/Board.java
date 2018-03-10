6a411983d9f9a9a9e620f9d0a1b434d1c772ab21
sbai <shufengbai@gmail.com>
2018-03-10 03:19:39 -0800

    minimally working game

--- app/src/main/java/com/example/paul/aklotski/Board.java
+++ app/src/main/java/com/example/paul/aklotski/Board.java
@@ -1,5 +1,8 @@
 package com.example.paul.aklotski;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by Paul on 3/9/2018.
  * This is a board and status
@@ -24,8 +27,73 @@ public class Board extends Object {
             }
         }
     }
+    enum Direction {U, D, L, R};
+    public void movePiece(int index)
+    {
+        // pupulate board;
+        int [][] board = new int [H][W];
+        for(int i = 0; i < blocks.length; ++i)
+        {
+            if( i == index)
+                continue;
+            Block b = blocks[i];
+            for(int y = 0; y < b.H(); ++y)
+                for(int x = 0; x < b.W(); ++x)
+                    board[b.y+y][b.x+x] = i+1; // so King will also block
+        }
+        ArrayList<Direction> direction = new ArrayList<Direction>();
+        Block b = blocks[index];
+        if(b.y > 0)
+        {
+            boolean open  = true;
+            for (int x = 0; x < b.W(); ++x)
+                if(board[b.y-1][b.x+x] > 0)
+                    open = false;
+            if (open)
+                direction.add(Direction.U);
+        }
+        if(b.y+b.H() < H)
+        {
+            boolean open  = true;
+            for (int x = 0; x < b.W(); ++x)
+                if(board[b.y+b.H()][b.x+x] > 0)
+                    open = false;
+            if (open)
+                direction.add(Direction.D);
+        }
+        if(b.x > 0)
+        {
+            boolean open  = true;
+            for (int y = 0; y < b.H(); ++y)
+                if(board[b.y+y][b.x-1] > 0)
+                    open = false;
+            if (open)
+                direction.add(Direction.L);
+        }
+        if(b.x + b.W()< W)
+        {
+            boolean open  = true;
+            for (int y = 0; y < b.H(); ++y)
+                if(board[b.y+y][b.x+b.W()] > 0)
+                    open = false;
+            if (open)
+                direction.add(Direction.R);
+        }
+        if(direction.size() > 0)
+        {
+            Random rand = new Random();
+            int value = rand.nextInt(direction.size());
+            Direction d = direction.get(value);
+            switch (d) {
+                case U :  --b.y; break;
+                case D :  ++b.y; break;
+                case L :  --b.x; break;
+                case R :  ++b.x; break;
+            }
+        }
+    }
     static final int W = 4;
     static final int H = 5;
     Block[] blocks = new Block[10];
-    char [][] board;
+    //char [][] board;
 }
--- app/src/main/java/com/example/paul/aklotski/MainActivity.java
+++ app/src/main/java/com/example/paul/aklotski/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
@@ -17,12 +18,13 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main); // use code ot set instead of xml
+        /*
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.drawable.forgotmenot);
         imageView.setScaleType(ImageView.ScaleType.FIT_XY);
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 FrameLayout.LayoutParams.WRAP_CONTENT);
         lp1.setMargins(0, 20, 0, 0);
         textView1.setLayoutParams(lp1);
-
+    */
         //Adding views to FrameLayout
         String state[] = new String[]{
                 "VKkV",
@@ -48,37 +50,43 @@ public class MainActivity extends AppCompatActivity {
                 "VHhV",
                 "vPPv",
                 "P  P"};
-        Board board = new Board(state);
+        board = new Board(state);
         BoardView bv = new BoardView(this, board);
         setContentView(bv);
     }
-}
-    class BlockView extends FrameLayout {
-        BlockView(Context context, Block block, int boardW, int boardH) {
-            super(context);
-            label = new TextView(getContext());
-            label.setTextSize(32);
-            label.setBackgroundColor(0xffbc4d05);
-            label.setTextColor(0x330D0D0D);
-            label.setGravity(Gravity.CENTER);
-            //LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 
+    @Override
+    public void onClick(View v) {
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(v.getWidth(), v.getHeight());
+        lp.setMargins(10, 10, 0, 0);
+        //((TextView)v).setText("C");\
+        board.movePiece(((BlockView)v).id);
+        BoardView bv = new BoardView(this, board);
+        setContentView(bv);
+    }
+    Board board;
+}
+    class BlockView extends TextView {
+        BlockView(Context context, Block b, int index) {
+            super(context);
+            block = b;
+            id = index;
+            setTextSize(32);
+            setBackgroundColor(0xffbc4d05);
+            setTextColor(0x330D0D0D);
+            setGravity(Gravity.CENTER);
             int W = 300;
             int H = 300;
             int G = 10;
-            LayoutParams lp = new LayoutParams(W * block.W() - 2*G, H * block.H()-2*G);
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(W * block.W() - 2*G, H * block.H()-2*G);
             int x1 = block.x * W + G;
-            int x2 = (block.x + block.W()) * W - G;
-            x2 = boardW * W - x2;
             int y1 = block.y * H + G;
-            int y2 = (block.y + block.H()) * H - G;
-            y2 = boardH * H - y2;
-            lp.setMargins(x1, y1, x2, y2);
-            label.setLayoutParams(lp);
-            label.setText(block.t.toString());
-            addView(label);
+            lp.setMargins(x1, y1, 0, 0);
+            setLayoutParams(lp);
+            setText(block.t.toString());
         }
-        TextView label;
+        int id;
+        Block block;
     }
 
     class BoardView extends FrameLayout {
@@ -88,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
             setLayoutParams(new AbsListView.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                     FrameLayout.LayoutParams.MATCH_PARENT));
             for (int i = 0; i < board.blocks.length; ++i) {
-                BlockView b = new BlockView(context, board.blocks[i], board.W, board.H);
+                BlockView b = new BlockView(context, board.blocks[i], i);
+                b.setOnClickListener((OnClickListener) context);
                 addView(b);
             }
         }
